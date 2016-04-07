15750f893ff36854895f734ad2ea49a35f72faae
russellvaughan <russellvaughan@gmail.com>
2016-04-07 08:58:02 +0100

    first commit

--- spec/features/statement_printing_spec.rb
+++
@@ -1,9 +0,0 @@
-# describe "Statement Printing" do 
-# 	it "displays a printed statement" do 
-# 		account = BankAccount.new
-# 		account.credit(1000, '10-01-2012')
-# 		account.credit(2000, '13-01-2012')
-# 		account.debit(500, '14-01-2012')
-# 		expect(account.print).to eq("date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00\n13/01/2012 || 2000.00 || || 3000.00\n10/01/2012 || 1000.00 || || 1000.00")
-# 	end
-# end
--- spec/features/withdraw_feature_test_spec.rb
+++
@@ -1,7 +0,0 @@
-	describe "Withdrawing money" do 
-		it 'allows a user to withdraw money from an account' do 
-			account = BankAccount.new(100)
-			account.debit(100)
-			expect(account.balance).to eq(0)
-		end
-	end
